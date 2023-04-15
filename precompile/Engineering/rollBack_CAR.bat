5afe69ca1daf785a2aa763de55a2fe9250e77e5c
Sai Prasad Joshi <saiprasadj@VALUEMOMENTUM.BIZ>
2023-04-15 21:16:59 +0530

    Files moved from Windows machine to GitHubb

--- precompile/Engineering/rollBack_CAR.bat
+++
@@ -1,3 +0,0 @@
-if not exist C:\deployments72\precompile\Engineering\CARBusinessRMA_service_0.adb ECHO Success
-REN "C:\deployments72\precompile\Engineering\Adb_Backup\CARBusinessRMA_service_%date:~-4,4%%date:~-7,2%%date:~-10,2%.adb" "CARBusinessRMA_service_0.adb"
-move /-y "C:\deployments72\precompile\Engineering\Adb_Backup\CARBusinessRMA_service_0.adb" "C:\deployments72\precompile\Engineering"
